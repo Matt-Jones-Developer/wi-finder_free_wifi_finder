5d0379a49c62fadb4d17aeb91b730064a87aec7a
Matt Jones <glitchyghost81@protonmail.com>
2023-03-10 19:24:20 +0000

    added skeletal directory structure and files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './pages/Main';
 
-function App() {
+import './App.css'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Header />
+      <Routes>
+        <Route path="/" element={ <Main /> } index={true} />
+        {/* <Route path="/new" element={ <New /> } /> */}
+      </Routes>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
+
+
