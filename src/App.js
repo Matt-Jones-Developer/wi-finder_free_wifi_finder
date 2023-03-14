16283850fbcf24cc83f6aa73f49b931e9bd5c666
marc hudson <marchudson2601@gmail.com>
2023-03-14 15:33:27 +0000

    api added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Header from './components/Header';
-import Footer from './components/Footer';
+// import NavBar from './components/NavBar';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
 import Main from './pages/Main';
-
+import "./Api"
 import './App.css'
 
 const App = () => {
   return (
     <Router>
-      <NavBar />
-      <Header />
+      {/* <NavBar /> */}
+      {/* <Header /> */}
       <Routes>
-        <Route path="/" element={ <Main /> } index={true} />
-        {/* <Route path="/new" element={ <New /> } /> */}
+        <Route path="/" element={<Main />} index={true} />
+        {/* <Route path="/new" element={<New />} /> */}
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   );
 };
