5cf3865150fdde0bbc3121ded1f97b996e804014
FOD <fionnuala.k.odonnell@gmail.com>
2023-03-16 14:44:38 -0400

    used tailwinds to style cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,34 @@
 import React from "react";
-//import "./style.css";
+import styles from "./styles/Card.module.css";
+import github_logo from "../assets/png/github-mark-white.png";
+import linkedIn_logo from "../assets/png/LI-In-Bug.png";
 
 
 function Card(props) {
 
   return (
-    <div>
+    <div  className = "bg-[#292C33] bg-opacity-75 m-4 p-6 rounded">
+      <div>
       <img src={props.profilePic} alt={props.name} ></img>
       <div>
-        <h3>{props.name}</h3>
-        <h2>{props.jobTitle}</h2>
+        <h2 className ="underline decoration-[#78E15B]">{props.name}</h2>
+        <h3 className ="mb-4" >{props.jobTitle}</h3>
         <p>{props.about}</p>
-          <div>
-            <a href={props.gitHubUrl}>
-              <img src="../assets/png/github-mark.png" alt="GitHub Logo"></img>
+          <div className = "pt-6 inline-flex">
+            <a className = "object-contain w-10 h-10 mr-4" href={props.gitHubUrl}>
+            <img
+            src={github_logo}
+            alt="GitHub Logo"
+          />
             </a>
-            <a href={props.linkedInUrl}>
-                <img src="../assets/png/LI-Logo.png" alt="LinedIn Logo"></img>
+            <a  className = "object-contain w-12 h-12" href={props.linkedInUrl}>
+            <img
+            src={linkedIn_logo}
+            alt="Linkedin Logo"
+            className=""
+          />
             </a>
+          </div>
           </div>
           </div>
           </div>
