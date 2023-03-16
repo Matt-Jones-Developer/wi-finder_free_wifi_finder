5e9ceb91ca37d734c72ed9475b524d123e6b49b1
Matt Jones <glitchyghost81@protonmail.com>
2023-03-16 16:06:13 +0000

    main page with search input form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import NavBar from './components/NavBar';
 // import Header from './components/Header';
 // import Footer from './components/Footer';
 import Main from './pages/Main';
-import "./Api"
+import "./data/Api"
 import './App.css'
 
 const App = () => {
   return (
     <Router basename="/wi-finder_free_wifi_finder">
+    {/* // <Router> */}
       <Routes>
         <Route path="/" element={<Main />} index={true} />
       </Routes>
