76b6cafe526d7e3c50a9750bf8f532b93fd6edbd
Kelvin Enaro <kelvinenaro@Kelvins-MacBook-Pro.local>
2023-03-17 17:34:38 +0000

    fixed display on contact page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import NavBar from './components/NavBar';
 // import Header from './components/Header';
 // import Footer from './components/Footer';
-import Main from './pages/Main';
-import "./Api"
-import './App.css'
+import Main from "./pages/Main";
+import "./Api";
+import "./App.css";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
     <Router basename="/wi-finder_free_wifi_finder">
       <Routes>
         <Route path="/" element={<Main />} index={true} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
-
-
-
