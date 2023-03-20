e8eb66a5865fbf37b3e877c4dc379ec541db9d79
FOD <fionnuala.k.odonnell@gmail.com>
2023-03-20 11:07:11 -0400

    changed the linkedin logo to white version and added additional styling to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import styles from "./styles/Card.module.css";
+import styles from "./styles/Card.module.css";
 import github_logo from "../assets/png/github-mark-white.png";
-import linkedIn_logo from "../assets/png/LI-In-Bug.png";
+import linkedIn_logo from "../assets/png/Li-In-White.png";
 import marcHudson from "../assets/jpg/marc_hudson_profile_pic.jpg"
 import kelvinEnaro from "../assets/jpg/kelvin_enaro_profile_pic.jpg"
 import fiODonnell from "../assets/jpg/fionnuala_odonnell_profile_pic.jpg";
@@ -14,7 +14,7 @@ const profileImages = {
   3: mattJones
 }
 
-// console.log(profileImages)
+
 
 function Card(props) {
 
@@ -22,17 +22,17 @@ function Card(props) {
     <div className="bg-[#292C33] bg-opacity-75 m-4 p-6 rounded">
       <div className="flex justify-between">
         <div className="w-3/5 ">
-          <h2>{props.name}</h2>
-          <h3 className="mb-6 font-bold text-lime" >{props.jobTitle}</h3>
+          <h2 className="leading-10 pb-6">{props.name}</h2>
+          <h3 className={`${styles.jobTile} leading-10 mb-6 font-bold text-lime`} >{props.jobTitle}</h3>
           <p>{props.about}</p>
-          <div className="pt-6 inline-flex">
-            <a className="object-contain w-10 h-10 mr-4" href={props.gitHubUrl}>
+          <div className="py-5 inline-flex">
+            <a className={`${styles.gitHub} object-contain w-10 h-10 mr-4`} href={props.gitHubUrl}>
               <img
                 src={github_logo}
                 alt="GitHub Logo"
               />
             </a>
-            <a className="object-contain w-12 h-12" href={props.linkedInUrl}>
+            <a className={`${styles.linkedIn} object-contain w-12 h-12`} href={props.linkedInUrl}>
               <img
                 src={linkedIn_logo}
                 alt="Linkedin Logo"
@@ -40,15 +40,17 @@ function Card(props) {
             </a>
           </div>
         </div>
-        <div className="inline-flex justify-items-end">
+       
+        <div className="inline-flex justify-items-end drop-shadow-2xl">
               <img 
-              className="object-contain w-40 h-40"
+              className={`${styles.profilePic} bg-contain bg-top h-40 w-40 grayscale rounded border-b-1`}
               src={profileImages[props.index]} 
               alt={props.name}
               >
-              </img>            
+              </img>          
             </div>
       </div>
+      <hr className ="border-purps border-4 rounded-lg"></hr>
     </div>
   );
 }
