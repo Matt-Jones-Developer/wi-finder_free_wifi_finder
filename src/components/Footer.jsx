867a5799ec05e68a028ff618c446c263b3048ae7
FOD <fionnuala.k.odonnell@gmail.com>
2023-03-21 08:42:30 -0400

    removed privacy link and added back in about

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
                 </li>
                 <li>
                     <a href="#" class="mr-4 hover:underline md:mr-6 ">Terms & Conditions</a>
